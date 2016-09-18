31b5ab4011e44fafce567d03dcb00a41064f8fe1
lanhao34 <lanhao34@gmail.com>
2016-09-18 15:11:51 +0000

    resize the window of picture

--- topo.py
+++ topo.py
@@ -3,7 +3,7 @@ import matplotlib
 import pylab as plt
 
 m = 5
-h = 2
+h = 3
 interval = h * 2 - 1
 N = interval * (m-1)
 
@@ -11,9 +11,9 @@ G=nx.Graph()
 G.add_nodes_from(range(N))
 G.add_edges_from([(i,(i+1)%N) for i in xrange(N)])
 for MEMS_ID in xrange((m-1)/2):
-    G.add_edges_from([(i,(i+h*(MEMS_ID+1))%N) for i in xrange(N)])
-# print G.edges()
-nx.draw_circular(G, with_labels=True, node_size=500, node_color='#6ab0de', alpha=1, font_size=14, font_color='#2980B9')
+    G.add_edges_from([(i,(i+interval*(MEMS_ID+1))%N) for i in xrange(N)])
+
+nx.draw_circular(G, with_labels=True, node_size=500, node_color='#6ab0de', alpha=1, font_size=14)
 mng = plt.get_current_fig_manager()
-figManager.window.showMaximized()
-plt.show(figsize=(500,500))
+mng.resize(500,500)
+plt.show()
